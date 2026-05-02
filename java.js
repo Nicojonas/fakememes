745e8bf9852494bad8b50bdc7599cfdf805d2eb3
Lawrence <kalyangolawrence70@gmail.com>
2026-05-02 03:08:14 -0700

    i have deleted some code that was on line 17

--- java.js
+++ java.js
@@ -14,30 +14,7 @@ navLinks.forEach(link => {
   });
 });
 
-/* ================= ACTIVE SECTION TRACKING ================= */
-const sections = document.querySelectorAll('section[id]');
 
-function setActiveLink() {
-  let scrollY = window.pageYOffset;
-
-  sections.forEach(section => {
-    const sectionHeight = section.offsetHeight;
-    const sectionTop = section.offsetTop - 120;
-    const sectionId = section.getAttribute('id');
-
-    if (
-      scrollY >= sectionTop &&
-      scrollY < sectionTop + sectionHeight
-    ) {
-      navLinks.forEach(link => {
-        link.classList.remove('active');
-        if (link.getAttribute('href') === '#' + sectionId) {
-          link.classList.add('active');
-        }
-      });
-    }
-  });
-}
 
 window.addEventListener('scroll', setActiveLink);
 
